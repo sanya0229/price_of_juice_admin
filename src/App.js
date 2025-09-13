de13f68b0461eee6cac4d32630c8d75665252895
Sasha <sashay29.11@gmail.com>
2025-09-13 22:11:03 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
             } />
             <Route path="/products" element={
               <ProtectedRoute>
-                <Products />
+                <Products/>
               </ProtectedRoute>
             } />
             <Route path="/text" element={
